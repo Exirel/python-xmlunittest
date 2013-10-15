ef7d2ae0572ddf17a3459aa55acdc20399dab403
Exirel <florian.strzelecki@gmail.com>
2013-10-16 00:15:38 +0200

    Add comment in test.py.

--- test.py
+++ test.py
@@ -6,33 +6,23 @@ from xmlunittest import XmlTestCase
 
 
 class TestXmlTestCase(unittest.TestCase):
-    """
+    """Test the XmlTestCase.
 
-        Basic test on an XML document.
+    The issue of testing TestCase can be tough as only error case can be
+    tested. So it is important to know what you really want to test.
 
-        This tests how the XML assertions work.
-
-        data = '''<?xml version="1.0" encoding="UTF-8" ?>
-        <root xmlns:prefix="uri">
-            <child att="1" prefix:nsAtt="x"/>
-            <child att="1" prefix:nsAtt="y"/>
-            <child att="1" prefix:nsAtt="z"/>
-        </root>
-        '''
-
-        doc = self.assertXmlDocument(data)
-        self.assertXmlNamespace(doc, 'prefix', 'uri')
-
-        for node in doc.xpath('//child'):
-            self.assertXmlAttribute(node, 'att', expected_value='1')
-            self.assertXmlAttribute(node, 'prefix:nsAtt',
-                                    expected_values=['x', 'y', 'z'])
+    XmlTestCase is developped using TDD: tests are written before functionnal
+    code. For each successful case a related error case is tested too.
 
     """
-
-    # -------------------------------------------------------------------------
-
     def test_assertXmlDocument(self):
+        """Asserts assertXmlDocument raises when data is invalid.
+
+        At this time, assertXmlDocument only test XML data is a valid XML
+        document, but it can be a fragment of XML only. This does not test
+        the XML declaration nor any doctype declaration.
+
+        """
         test_case = XmlTestCase(methodName='assertXmlDocument')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
         <root/>"""
@@ -46,6 +36,12 @@ class TestXmlTestCase(unittest.TestCase):
     # -------------------------------------------------------------------------
 
     def test_assertXmlNamespace(self):
+        """Asserts assertXmlNamespace raises namespace is invalid.
+
+        When an element declare an xml namespace, this element and each child
+        reference this namespace, and thus it can be tested.
+
+        """
         test_case = XmlTestCase(methodName='assertXmlNamespace')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
         <root xmlns:ns="uri"/>"""
@@ -63,6 +59,12 @@ class TestXmlTestCase(unittest.TestCase):
     # -------------------------------------------------------------------------
 
     def test_assertXmlHasAttribute(self):
+        """Asserts assertXmlHasAttribute raises when attribute does not exist.
+
+        Method assertXmlHasAttribute can test if attribute exists or not, and
+        more - see other tests for that.
+
+        """
         test_case = XmlTestCase(methodName='assertXmlHasAttribute')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
         <root att="value" />"""
@@ -74,6 +76,12 @@ class TestXmlTestCase(unittest.TestCase):
             test_case.assertXmlHasAttribute(root, 'no_att')
 
     def test_assertXmlHasAttribute_value(self):
+        """Asserts assertXmlHasAttribute raises when value is invalid.
+
+        With optional argument `expected_value`, assertXmlHasAttribute can
+        assert if attribute's value is the given expected value.
+
+        """
         test_case = XmlTestCase(methodName='assertXmlHasAttribute')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
         <root att="value" />"""
@@ -86,6 +94,12 @@ class TestXmlTestCase(unittest.TestCase):
                                             expected_value='invalid')
 
     def test_assertXmlHasAttribute_values(self):
+        """Asserts assertXmlHasAttribute raises when value is invalid.
+
+        With optional argument `expected_values`, assertXmlHasAttribute can
+        assert if attribute's value is one of the given expected values.
+
+        """
         test_case = XmlTestCase(methodName='assertXmlHasAttribute')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
         <root>
@@ -106,7 +120,31 @@ class TestXmlTestCase(unittest.TestCase):
     # -------------------------------------------------------------------------
 
     def test_assertXmlNode(self):
+        """Asserts assertXmlNode raises when node is invalid.
+
+        Method assertXmlNode raise if node does not exists (None) or is not
+        an XML Element.
+
         """
+        test_case = XmlTestCase(methodName='assertXmlNode')
+        data = """<?xml version="1.0" encoding="UTF-8" ?>
+        <root>text_value</root>"""
+
+        root = test_case.assertXmlDocument(data)
+        test_case.assertXmlNode(root)
+
+        with self.assertRaises(test_case.failureException):
+            test_case.assertXmlNode(None)
+
+        # Text data is not handled
+        with self.assertRaises(test_case.failureException):
+            test_case.assertXmlNode('<root>text_value</root>')
+
+    def test_assertXmlNode_tag(self):
+        """Asserts assertXmlNode raises when node is invalid.
+
+        Method assertXmlNode raise if node has not the expected tag name.
+
         """
         test_case = XmlTestCase(methodName='assertXmlNode')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
@@ -114,26 +152,59 @@ class TestXmlTestCase(unittest.TestCase):
 
         root = test_case.assertXmlDocument(data)
 
-        test_case.assertXmlNode(root)
         test_case.assertXmlNode(root, tag='root')
+        with self.assertRaises(test_case.failureException):
+            test_case.assertXmlNode(root, tag='noRoot')
+
+    def test_assertXmlNode_text(self):
+        """Asserts assertXmlNode raises when node is invalid.
+
+        Method assertXmlNode raise if node has not the expected text value.
+
+        """
+        test_case = XmlTestCase(methodName='assertXmlNode')
+        data = """<?xml version="1.0" encoding="UTF-8" ?>
+        <root>text_value</root>"""
+
+        root = test_case.assertXmlDocument(data)
+
         test_case.assertXmlNode(root, text='text_value')
+        with self.assertRaises(test_case.failureException):
+            test_case.assertXmlNode(root, text='invalid')
+
+    def test_assertXmlNode_tag_text(self):
+        """Asserts assertXmlNode raises when node is invalid.
+
+        Method assertXmlNode raise if node has not the expected tag name
+        or the expected text value.
+
+        """
+        test_case = XmlTestCase(methodName='assertXmlNode')
+        data = """<?xml version="1.0" encoding="UTF-8" ?>
+        <root>text_value</root>"""
+
+        root = test_case.assertXmlDocument(data)
+
         test_case.assertXmlNode(root, tag='root', text='text_value')
 
         with self.assertRaises(test_case.failureException):
-            test_case.assertXmlNode(root, tag='noRoot')
+            test_case.assertXmlNode(root, tag='root', text='invalid')
 
         with self.assertRaises(test_case.failureException):
             test_case.assertXmlNode(root, tag='noRoot', text='text_value')
 
         with self.assertRaises(test_case.failureException):
-            test_case.assertXmlNode(root, text='invalid')
-
-        with self.assertRaises(test_case.failureException):
-            test_case.assertXmlNode(root, tag='root', text='invalid')
+            test_case.assertXmlNode(root, tag='noRoot', text='invalid')
 
     # -------------------------------------------------------------------------
 
     def test_assertXpathExistForEachNodes(self):
+        """Asserts assertXpathExistForEachNodes raises when validation failed.
+
+        Method assertXpathExistForEachNodes raises when the list of XPath
+        expressions returns None for any node of the given nodes.
+
+        """
         test_case = XmlTestCase(methodName='assertXpathExistForEachNodes')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
         <root>
@@ -167,6 +238,12 @@ class TestXmlTestCase(unittest.TestCase):
     # -------------------------------------------------------------------------
 
     def test_assertXpathUniqueForEachNodes(self):
+        """Asserts assertXpathUniqueForEachNodes raises when validation failed.
+
+        Method assertXpathUniqueForEachNodes raises when one of XPath
+        expressions returns does not returns one result and exactly one result.
+
+        """
         test_case = XmlTestCase(methodName='assertXpathUniqueForEachNodes')
         data = """<?xml version="1.0" encoding="UTF-8" ?>
         <root>
