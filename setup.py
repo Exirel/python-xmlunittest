2c0da4277f97837611119dc0f8875a63cdf6cf8b
Florian Strzelecki <florian.strzelecki@gmail.com>
2013-11-23 19:57:07 +0100

    Add manifest and setup.py for install

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(name='xmlunittest',
+      version='0.1.0',
+      description='Library using lxml and unittest for unit testing XML.',
+      author='Florian Strzelecki',
+      author_email='florian.strzelecki@gmail.com',
+      license='MIT',
+      url='https://github.com/Exirel/python-xmlunittest',
+      py_modules=['xmlunittest'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Software Development :: Testing',
+                   'Topic :: Text Processing :: Markup :: XML'])
